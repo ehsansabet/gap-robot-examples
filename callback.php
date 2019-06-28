3af09a7cb77fac0a6a395cec63246e317b414cbf
Ehsan Sabet <ehsan.sabet@hotmail.com>
2019-06-28 14:43:44 +0430

    inline calendar

---
+++ callback.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Author: Ehsan Sabet (ehsan.sabet@hotmail.com)
+ */
+
+require dirname( __FILE__ ) . '/vendor/autoload.php';
+
+use Gap\SDP\Api;
+use ehsansabet\GapUtils\InlineCalendar;
+
+$token = 'c4e972b152b01ff0a23d7500d3356a2d2c3f504e5c9961d1028995d932a19b2e';
+try {
+	$gap = new Api( $token );
+} catch ( Exception $e ) {
+	throw new \Exception( 'an error was encountered' );
+}
+
+$data    = isset( $_POST['data'] ) ? $_POST['data'] : null;
+$type    = isset( $_POST['type'] ) ? $_POST['type'] : null;
+$chat_id = isset( $_POST['chat_id'] ) ? $_POST['chat_id'] : null;
+$from    = isset( $_POST['from'] ) ? $_POST['from'] : null;
+
+if ( $data == '/calendar' ) {
+	$InlineKeyboard = InlineCalendar::show();
+
+	return $gap->sendText( $chat_id, 'تقویم', null, $InlineKeyboard );
+}
+
+if ( $type == 'triggerButton' ) {
+	$trigger = json_decode( $data );
+	$btnData = explode( '_', $trigger->data );
+
+	if ( $btnData[0] == 'gotoMonth' ) {
+		$calendarConfig = [
+			'format' => $btnData[1]
+		];
+		$InlineKeyboard = InlineCalendar::show( $calendarConfig );
+		try {
+			return $gap->editMessage( $chat_id, (int) $trigger->message_id, null, $InlineKeyboard );
+		} catch ( Exception $e ) {
+			return;
+		}
+	}
+
+	if ( $trigger->data == 'null' ) {
+		return $gap->answerCallback( $chat_id, $trigger->callback_id, 'none', false );
+	}
+
+	return $gap->answerCallback( $chat_id, $trigger->callback_id, $trigger->data, true );
+}
+
+return $gap->sendText( $chat_id, 'برای نمایش تقویم /calendar را ارسال کنید.' );
